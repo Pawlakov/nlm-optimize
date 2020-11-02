2c119c83f85b4e6ad8b0873c6210363d4afe14d9
Paweł Matusek <pmatusek1@wp.pl>
2020-11-02 20:19:17 +0100

    HEEELP me im daed

--- NLMBase/Implementation.cs
+++ NLMBase/Implementation.cs
@@ -2,15 +2,45 @@ namespace NLMBase
 {
     public unsafe class Implementation
     {
-        public void Denoise(byte* inputPointer, byte* outputPointer, int length, int h)
+        public void Denoise(byte* inputPointer, byte* outputPointer, int length, double h)
         {
             for (var i = 0; i < length; ++i)
             {
+                var c = CalculateC(i, length);
+                var sum = 0.0;
                 for (var j = 0; j < length; ++j)
                 {
-                    
+                    sum += inputPointer[j] * CalculateF(i, j, length);
                 }
+
+                var newValue = sum / c;
             }
+        }
+
+        private double CalculateC(int pixelIndex, int length)
+        {
+            var sum = 0.0;
+            for (var i = 0; i < length; ++i)
+            {
+                sum += CalculateF(pixelIndex, i, length);
+            }
+
+            return sum;
+        }
+
+        private double CalculateF(int pixelIndex, int otherPixelIndex, int length)
+        {
+
+        }
+
+        private double CalculateB(int pixelIndex)
+        {
+
+        }
+
+        private double CalculateR()
+        {
+
         }
     }
 }
