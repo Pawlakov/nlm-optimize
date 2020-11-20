3ceabca16c542e121793b30935b30384f56d595e
Pawlakov <pmatusek1@wp.pl>
2020-11-20 16:13:08 +0100

    piątek 20 listopada

--- NLMImplem/NLMGpgpu.cpp
+++ NLMImplem/NLMGpgpu.cpp
@@ -2,250 +2,100 @@
 
 extern void Denoise(int iDWin, int iDBloc, float fSigma, float fFiltPar, float** fpI, float** fpO, int iChannels, int iWidth, int iHeight)
 {
-	// length of each channel
-	int iwxh = iWidth * iHeight;
-
-	//  length of comparison window
-	int ihwl = (2 * iDWin + 1);
-	int iwl = (2 * iDWin + 1) * (2 * iDWin + 1);
-	int icwl = iChannels * iwl;
-
-	// filtering parameter
-	float fSigma2 = fSigma * fSigma;
-	float fH = fFiltPar * fSigma;
-	float fH2 = fH * fH;
-
-	// multiply by size of patch, since distances are not normalized
-	fH2 *= (float)icwl;
-
-	// tabulate exp(-x), faster than using directly function expf
-	int iLutLength = (int)rintf((float)LUTMAX * (float)LUTPRECISION);
-	float* fpLut = new float[iLutLength];
-	wxFillExpLut(fpLut, iLutLength);
-
-	// auxiliary variable
-	// number of denoised values per pixel
-	float* fpCount = new float[iwxh];
-	fpClear(fpCount, 0.0f, iwxh);
-
-	// clear output
-	for (int ii = 0; ii < iChannels; ii++)
-	{
-		fpClear(fpO[ii], 0.0f, iwxh);
-	}
-
-	// PROCESS STARTS
-	// for each pixel (x,y)
-#pragma omp parallel shared(fpI, fpO)
-	{
-#pragma omp for schedule(dynamic) nowait
-		for (int y = 0; y < iHeight; y++)
-		{
-			// auxiliary variable
-			// denoised patch centered at a certain pixel
-			float** fpODenoised = new float* [iChannels];
-			for (int ii = 0; ii < iChannels; ii++)
-			{
-				fpODenoised[ii] = new float[iwl];
-			}
-
-			for (int x = 0; x < iWidth; x++)
-			{
-				// reduce the size of the comparison window if we are near the boundary
-				int iDWin0 = MIN(iDWin, MIN(iWidth - 1 - x, MIN(iHeight - 1 - y, MIN(x, y))));
-
-				// research zone depending on the boundary and the size of the window
-				int imin = MAX(x - iDBloc, iDWin0);
-				int jmin = MAX(y - iDBloc, iDWin0);
-
-				int imax = MIN(x + iDBloc, iWidth - 1 - iDWin0);
-				int jmax = MIN(y + iDBloc, iHeight - 1 - iDWin0);
-
-				//  clear current denoised patch
-				for (int ii = 0; ii < iChannels; ii++) fpClear(fpODenoised[ii], 0.0f, iwl);
-
-				// maximum of weights. Used for reference patch
-				float fMaxWeight = 0.0f;
-
-				// sum of weights
-				float fTotalWeight = 0.0f;
-
-				for (int j = jmin; j <= jmax; j++)
-				{
-					for (int i = imin; i <= imax; i++)
-					{
-						if (i != x || j != y)
-						{
-							float fDif = fiL2FloatDist(fpI, fpI, x, y, i, j, iDWin0, iChannels, iWidth, iWidth);
-
-							// dif^2 - 2 * fSigma^2 * N      dif is not normalized
-							fDif = MAX(fDif - 2.0f * (float)icwl * fSigma2, 0.0f);
-							fDif = fDif / fH2;
-
-							float fWeight = wxSLUT(fDif, fpLut);
-
-							if (fWeight > fMaxWeight)
-							{
-								fMaxWeight = fWeight;
-							}
-
-							fTotalWeight += fWeight;
-
-							for (int is = -iDWin0; is <= iDWin0; is++)
-							{
-								int aiindex = (iDWin + is) * ihwl + iDWin;
-								int ail = (j + is) * iWidth + i;
-
-								for (int ir = -iDWin0; ir <= iDWin0; ir++)
-								{
-									int iindex = aiindex + ir;
-									int il = ail + ir;
-
-									for (int ii = 0; ii < iChannels; ii++)
-									{
-										fpODenoised[ii][iindex] += fWeight * fpI[ii][il];
-									}
-								}
-							}
-						}
-					}
-				}
-
-				// current patch with fMaxWeight
-				for (int is = -iDWin0; is <= iDWin0; is++)
-				{
-					int aiindex = (iDWin + is) * ihwl + iDWin;
-					int ail = (y + is) * iWidth + x;
-					for (int ir = -iDWin0; ir <= iDWin0; ir++)
-					{
-						int iindex = aiindex + ir;
-						int il = ail + ir;
-
-						for (int ii = 0; ii < iChannels; ii++)
-						{
-							fpODenoised[ii][iindex] += fMaxWeight * fpI[ii][il];
-						}
-					}
-				}
-
-				fTotalWeight += fMaxWeight;
-
-				// normalize average value when fTotalweight is not near zero
-				if (fTotalWeight > fTiny)
-				{
-					for (int is = -iDWin0; is <= iDWin0; is++)
-					{
-						int aiindex = (iDWin + is) * ihwl + iDWin;
-						int ail = (y + is) * iWidth + x;
-
-						for (int ir = -iDWin0; ir <= iDWin0; ir++)
-						{
-							int iindex = aiindex + ir;
-							int il = ail + ir;
-
-							fpCount[il]++;
-
-							for (int ii = 0; ii < iChannels; ii++)
-							{
-								fpO[ii][il] += fpODenoised[ii][iindex] / fTotalWeight;
-							}
-						}
-					}
-				}
-			}
-
-			for (int ii = 0; ii < iChannels; ii++)
-			{
-				delete[] fpODenoised[ii];
-			}
-
-			delete[] fpODenoised;
-		}
-	}
-
-	for (int ii = 0; ii < iwxh; ii++)
-	{
-		if (fpCount[ii] > 0.0)
-		{
-			for (int jj = 0; jj < iChannels; jj++)
-			{
-				fpO[jj][ii] /= fpCount[ii];
-			}
-		}
-		else
-		{
-			for (int jj = 0; jj < iChannels; jj++)
-			{
-				fpO[jj][ii] = fpI[jj][ii];
-			}
-		}
-	}
-
-	// delete memory
-	delete[] fpLut;
-	delete[] fpCount;
-}
-
-void fpClear(float* fpI, float fValue, int iLength)
-{
-	for (int ii = 0; ii < iLength; ii++)
-	{
-		fpI[ii] = fValue;
-	}
-}
-
-// LUT tables
-void  wxFillExpLut(float* lut, int size)
-{
-	for (int i = 0; i < size; i++)
-	{
-		lut[i] = expf(-(float)i / LUTPRECISION);
-	}
-}
-
-float wxSLUT(float dif, float* lut)
-{
-	if (dif >= (float)LUTMAXM1)
-	{
-		return 0.0;
-	}
-
-	int  x = (int)floor((double)dif * (float)LUTPRECISION);
-
-	float y1 = lut[x];
-	float y2 = lut[x + 1];
-
-	return y1 + (y2 - y1) * (dif * LUTPRECISION - x);
-}
-
-float fiL2FloatDist(float* u0, float* u1, int i0, int j0, int i1, int j1, int radius, int width0, int width1)
-{
-	float dist = 0.0;
-	for (int s = -radius; s <= radius; s++)
-	{
-		int l = (j0 + s) * width0 + (i0 - radius);
-		float* ptr0 = &u0[l];
-
-		l = (j1 + s) * width1 + (i1 - radius);
-		float* ptr1 = &u1[l];
-
-		for (int r = -radius; r <= radius; r++, ptr0++, ptr1++)
-		{
-			float dif = (*ptr0 - *ptr1);
-			dist += (dif * dif);
-		}
-	}
-
-	return dist;
-}
-
-float fiL2FloatDist(float** u0, float** u1, int i0, int j0, int i1, int j1, int radius, int channels, int width0, int width1)
-{
-	float dif = 0.0f;
-	for (int ii = 0; ii < channels; ii++)
-	{
-		dif += fiL2FloatDist(u0[ii], u1[ii], i0, j0, i1, j1, radius, width0, width1);
-	}
-
-	return dif;
+	// Create the two input vectors
+    int i;
+    const int LIST_SIZE = 1024;
+    int *A = (int*)malloc(sizeof(int)*LIST_SIZE);
+    int *B = (int*)malloc(sizeof(int)*LIST_SIZE);
+    for(i = 0; i < LIST_SIZE; i++) {
+        A[i] = i;
+        B[i] = LIST_SIZE - i;
+    }
+ 
+    // Load the kernel source code into the array source_str
+    FILE *fp;
+    char *source_str;
+    size_t source_size;
+ 
+    fp = fopen("vector_add_kernel.cl", "r");
+    if (!fp) {
+        fprintf(stderr, "Failed to load kernel.\n");
+        exit(1);
+    }
+    source_str = (char*)malloc(MAX_SOURCE_SIZE);
+    source_size = fread( source_str, 1, MAX_SOURCE_SIZE, fp);
+    fclose( fp );
+ 
+    // Get platform and device information
+    cl_platform_id platform_id = NULL;
+    cl_device_id device_id = NULL;   
+    cl_uint ret_num_devices;
+    cl_uint ret_num_platforms;
+    cl_int ret = clGetPlatformIDs(1, &platform_id, &ret_num_platforms);
+    ret = clGetDeviceIDs( platform_id, CL_DEVICE_TYPE_DEFAULT, 1, 
+            &device_id, &ret_num_devices);
+ 
+    // Create an OpenCL context
+    cl_context context = clCreateContext( NULL, 1, &device_id, NULL, NULL, &ret);
+ 
+    // Create a command queue
+    cl_command_queue command_queue = clCreateCommandQueue(context, device_id, 0, &ret);
+ 
+    // Create memory buffers on the device for each vector 
+    cl_mem a_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY, 
+            LIST_SIZE * sizeof(int), NULL, &ret);
+    cl_mem b_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY,
+            LIST_SIZE * sizeof(int), NULL, &ret);
+    cl_mem c_mem_obj = clCreateBuffer(context, CL_MEM_WRITE_ONLY, 
+            LIST_SIZE * sizeof(int), NULL, &ret);
+ 
+    // Copy the lists A and B to their respective memory buffers
+    ret = clEnqueueWriteBuffer(command_queue, a_mem_obj, CL_TRUE, 0,
+            LIST_SIZE * sizeof(int), A, 0, NULL, NULL);
+    ret = clEnqueueWriteBuffer(command_queue, b_mem_obj, CL_TRUE, 0, 
+            LIST_SIZE * sizeof(int), B, 0, NULL, NULL);
+ 
+    // Create a program from the kernel source
+    cl_program program = clCreateProgramWithSource(context, 1, 
+            (const char **)&source_str, (const size_t *)&source_size, &ret);
+ 
+    // Build the program
+    ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
+ 
+    // Create the OpenCL kernel
+    cl_kernel kernel = clCreateKernel(program, "vector_add", &ret);
+ 
+    // Set the arguments of the kernel
+    ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&a_mem_obj);
+    ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&b_mem_obj);
+    ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&c_mem_obj);
+ 
+    // Execute the OpenCL kernel on the list
+    size_t global_item_size = LIST_SIZE; // Process the entire lists
+    size_t local_item_size = 64; // Divide work items into groups of 64
+    ret = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, 
+            &global_item_size, &local_item_size, 0, NULL, NULL);
+ 
+    // Read the memory buffer C on the device to the local variable C
+    int *C = (int*)malloc(sizeof(int)*LIST_SIZE);
+    ret = clEnqueueReadBuffer(command_queue, c_mem_obj, CL_TRUE, 0, 
+            LIST_SIZE * sizeof(int), C, 0, NULL, NULL);
+ 
+    // Display the result to the screen
+    for(i = 0; i < LIST_SIZE; i++)
+        printf("%d + %d = %d\n", A[i], B[i], C[i]);
+ 
+    // Clean up
+    ret = clFlush(command_queue);
+    ret = clFinish(command_queue);
+    ret = clReleaseKernel(kernel);
+    ret = clReleaseProgram(program);
+    ret = clReleaseMemObject(a_mem_obj);
+    ret = clReleaseMemObject(b_mem_obj);
+    ret = clReleaseMemObject(c_mem_obj);
+    ret = clReleaseCommandQueue(command_queue);
+    ret = clReleaseContext(context);
+    free(A);
+    free(B);
+    free(C);
 }
--- NLMImplem/NLMGpgpu.h
+++ NLMImplem/NLMGpgpu.h
@@ -1,27 +1,7 @@
 #pragma once
 
-#define _USE_MATH_DEFINES
-#include <cmath>
+#include <stdio.h>
+#include <stdlib.h>
+#include <CL/cl.h>
+
 #include "NLM.h"
-
-#define MAX(i,j) ( (i)<(j) ? (j):(i) )
-#define MIN(i,j) ( (i)<(j) ? (i):(j) )
-
-#define dTiny 1e-10
-#define fTiny 0.00000001f
-#define fLarge 100000000.0f
-
-///// LUT tables
-#define LUTMAX 30.0
-#define LUTMAXM1 29.0
-#define LUTPRECISION 1000.0
-
-void  wxFillExpLut(float* lut, int size);        // Fill exp(-x) lut
-
-float wxSLUT(float dif, float* lut);                     // look at LUT
-
-void fpClear(float* fpI, float fValue, int iLength);
-
-float fiL2FloatDist(float* u0, float* u1, int i0, int j0, int i1, int j1, int radius, int width0, int width1);
-
-float fiL2FloatDist(float** u0, float** u1, int i0, int j0, int i1, int j1, int radius, int channels, int width0, int width1);
