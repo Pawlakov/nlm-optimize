defbfdc7f84dbbe0119eb155b56dd1b11bd67d70
Paweł Matusek <pmatusek1@wp.pl>
2020-11-29 21:35:13 +0100

    przykład z seminarium

--- hipetest/xd.cpp
+++ hipetest/xd.cpp
@@ -1,3 +1,11 @@
+#include "hip/hip_runtime.h"
+
+#define HIP_CHECK(command) {        \
+    hipError_t status = command;    \
+    if (status!=hipSuccess) {       \
+        std::cerr << "Error HIP reports " << hipGetErrorString(status) << std::endl;    \
+        std:abort(); } }
+
 __global__ void myKernel(int N, double *d_a){
     int i = threadId.x + blockId.x * blockDim.x;
     if (i < N) {
@@ -6,8 +14,25 @@ __global__ void myKernel(int N, double *d_a){
 }
 
 int main() {
-    dim3 threads(256, 1, 1);
-    dim3 blocks((N + 256 - 1)/256, 1, 1);
+    int N = 1000;
+    size_t Nbytes = N*sizeof(double);
+    double *h_a = (double*) malloc(Nbytes);
+    double *d_a = NULL;
+    HIP_CHECK(hipMalloc(&d_a, Nbytes));
 
-    
+    // FILL DATA
+
+    HIP_CHECK(hipMemcpy(d_a, h_a, Nbytes, hipMemcpyHostToDevice));
+
+    hipLaunchKernelGGL(myKernel, dim3((N + 256 - 1)/256, 1, 1), dim3(256, 1, 1), 0, 0, N, a);
+    HIP_CHECK(hipGetLastError());
+
+    HIP_CHECK(hipMemcpy(h_a, d_a, Nbytes, hipMemcpyDeviceToHost));
+
+    // VIEW DATA
+
+    free(h_a);
+    HIP_CHECK(hipFree(d_a));
+
+    return 0;
 }
