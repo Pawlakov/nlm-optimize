8a7167d8efccae6e1969890e1da0391b97b01ec5
Pawlakov <pmatusek1@wp.pl>
2020-12-17 22:35:24 +0100

    działająca wersja na gpu

--- NLMImplem/NLMGpgpu.hip.cpp
+++ NLMImplem/NLMGpgpu.hip.cpp
@@ -1,275 +1,234 @@
 #include "NLMGpgpu.hip.h"
 
-__device__ float wxSLUT(float dif, float* lut)
+// suma podniesionych do kwadratu różnic pomiędzy poszczegółnymi pikselami w dwóch oknach
+float fiL2FloatDist(float** input, int x0, int y0, int x1, int y1, int windowRadius, int channels, int width)
 {
-	if (dif >= (float)LUTMAXM1)
+	float sum = 0.0f;
+	for (int z = 0; z < channels; z++)
 	{
-		return 0.0;
+		for (int y = -windowRadius; y <= windowRadius; y++)
+		{
+			for (int x = -windowRadius; x <= windowRadius; x++)
+			{
+				int l0 = (y0 + y) * width + x0 + x;
+				int l1 = (y1 + y) * width + x1 + x;
+				float difference = (input[z][l0] - input[z][l1]);
+				sum += (difference * difference);
+			}
+		}
 	}
 
-	int  x = (int)floor((double)dif * (float)LUTPRECISION);
-
-	float y1 = lut[x];
-	float y2 = lut[x + 1];
-
-	return y1 + (y2 - y1) * (dif * LUTPRECISION - x);
+	return sum;
 }
 
-__device__ float fiL2FloatDist(float* u0, float* u1, int i0, int j0, int i1, int j1, int radius, int width0, int width1)
+__device__ float fiL2FloatDist(float* input, int x0, int y0, int x1, int y1, int windowRadius, int channels, int width, int channelLength)
 {
-	float dist = 0.0;
-	for (int s = -radius; s <= radius; s++)
+	float sum = 0.0f;
+	for (int z = 0; z < channels; z++)
 	{
-		int l = (j0 + s) * width0 + (i0 - radius);
-        int ptr0 = l;
-
-        l = (j1 + s) * width1 + (i1 - radius);
-        int ptr1 = l;
-
-        for (int r = -radius; r <= radius; r++, ptr0++, ptr1++)
-        {
-            float dif = u0[ptr0] - u1[ptr1];
-            dist += (dif * dif);
-        }
+		for (int y = -windowRadius; y <= windowRadius; y++)
+		{
+			for (int x = -windowRadius; x <= windowRadius; x++)
+			{
+				int l0 = (y0 + y) * width + x0 + x;
+				int l1 = (y1 + y) * width + x1 + x;
+				float difference = (input[z * channelLength + l0] - input[z * channelLength + l1]);
+				sum += (difference * difference);
+			}
+		}
 	}
 
-	return dist;
+	return sum;
 }
 
-__device__ float fiL2FloatDist(float* u0, float* u1, int i0, int j0, int i1, int j1, int radius, int channels, int width0, int width1, int iwxh)
+__global__ void getGlobalWeightsKernel(float* totalWeights, int windowRadius, int blockRadius, float sigma, float filteringParam, float* input, int channels, int width, int height)
 {
-	float dif = 0.0f;
-	for (int ii = 0; ii < channels; ii++)
+	int xy = threadIdx.x + blockIdx.x * blockDim.x;
+	int x = xy % width;
+	int y = xy / width;
+
+	int channelLength = width * height;
+
+	int windowLength1D = 2 * windowRadius + 1;
+	int windowLength2D = windowLength1D * windowLength1D;
+	int windowLength3D = channels * windowLength2D;
+
+	int windowRadiusReduced = MIN(windowRadius, MIN(width - 1 - x, MIN(height - 1 - y, MIN(x, y))));
+
+	int blockXMin = MAX(x - blockRadius, windowRadiusReduced);
+	int blockYMin = MAX(y - blockRadius, windowRadiusReduced);
+
+	int blockXMax = MIN(x + blockRadius, width - 1 - windowRadiusReduced);
+	int blockYMax = MIN(y + blockRadius, height - 1 - windowRadiusReduced);
+
+	totalWeights[xy] = 0.0f;
+
+	for (int blockY = blockYMin; blockY <= blockYMax; blockY++)
 	{
-		dif += fiL2FloatDist(&u0[ii * iwxh], &u1[ii * iwxh], i0, j0, i1, j1, radius, width0, width1);
+		for (int blockX = blockXMin; blockX <= blockXMax; blockX++)
+		{
+			float difference = fiL2FloatDist(input, x, y, blockX, blockY, windowRadiusReduced, channels, width, channelLength);
+
+			difference = MAX(difference - 2.0f * windowLength3D * sigma * sigma, 0.0f);
+			difference = difference / (filteringParam * filteringParam * sigma * sigma * windowLength3D);
+
+			float weight = expf(-difference);
+
+			totalWeights[xy] += weight;
+		}
+	}
+}
+
+__global__ void getItDoneKernel(int windowRadius, int blockRadius, float sigma, float filteringParam, float* input, float* output, int channels, int width, int height, float* totalWeights)
+{
+	int outputXY = threadIdx.x + blockIdx.x * blockDim.x;
+	int count = 0;
+
+	int channelLength = width * height;
+
+	int windowLength1D = 2 * windowRadius + 1;
+	int windowLength2D = windowLength1D * windowLength1D;
+	int windowLength3D = channels * windowLength2D;
+
+	for (int z = 0; z < channels; z++)
+	{
+		output[z * channelLength + outputXY] = 0.0f;
 	}
 
-	return dif;
+	for (int windowYOffset = -windowRadius; windowYOffset <= windowRadius; windowYOffset++)
+	{
+		for (int windowXOffset = -windowRadius; windowXOffset <= windowRadius; windowXOffset++)
+		{
+			int inputXY = outputXY - windowXOffset - (windowYOffset * width);
+			int inputX = inputXY % width;
+			int inputY = inputXY / width;
+
+			int windowRadiusReduced = MIN(windowRadius, MIN(width - 1 - inputX, MIN(height - 1 - inputY, MIN(inputX, inputY))));
+
+			if (inputXY > 0 && inputXY < channelLength && totalWeights[inputXY] > fTiny && abs(windowYOffset) <= windowRadiusReduced && abs(windowXOffset) <= windowRadiusReduced)
+			{
+				int blockXMin = MAX(inputX - blockRadius, windowRadiusReduced);
+				int blockJMin = MAX(inputY - blockRadius, windowRadiusReduced);
+
+				int blockXMax = MIN(inputX + blockRadius, width - 1 - windowRadiusReduced);
+				int blockYMax = MIN(inputY + blockRadius, height - 1 - windowRadiusReduced);
+
+				for (int blockY = blockJMin; blockY <= blockYMax; blockY++)
+				{
+					for (int blockX = blockXMin; blockX <= blockXMax; blockX++)
+					{
+						float difference = fiL2FloatDist(input, inputX, inputY, blockX, blockY, windowRadiusReduced, channels, width, channelLength);
+
+						difference = MAX(difference - 2.0f * (float)windowLength3D * sigma * sigma, 0.0f);
+						difference = difference / (filteringParam * sigma * filteringParam * sigma * windowLength3D);
+
+						float weight = expf(-difference);
+
+						int inputValueXY = (blockY + windowYOffset) * width + blockX + windowXOffset;
+
+						if (blockX == inputX && blockY == inputY)
+						{
+							count++;
+						}
+
+						for (int z = 0; z < channels; z++)
+						{
+							output[z * channelLength + outputXY] += (weight * input[z * channelLength + inputValueXY] / totalWeights[inputXY]);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	if (count > 0)
+	{
+		for (int z = 0; z < channels; z++)
+		{
+			output[z * channelLength + outputXY] /= count;
+		}
+	}
+	else
+	{
+		for (int z = 0; z < channels; z++)
+		{
+			output[z * channelLength + outputXY] = input[z * channelLength + outputXY];
+		}
+	}
 }
 
-__global__ void theKernelOfShit(int iDWin, int iDBloc, float fSigma, float fFiltPar, float *fpI, float *fpO, int iChannels, int iWidth, int iHeight, float *fpLut)
+void getGlobalWeights(float* totalWeights, int windowRadius, int blockRadius, float sigma, float fFiltPar, float** input, int channels, int width, int height)
 {
-    int i = threadIdx.x + blockIdx.x * blockDim.x;
+	int channelLength = width * height;
 
-    // length of each channel
-    int iwxh = iWidth * iHeight;
+	dim3 gpuThreads(256, 1, 1);
+    dim3 gpuBlocks((channelLength + 256 - 1) / 256, 1, 1);
 
-    if (i < iwxh)
+	size_t channelBytes = channelLength * sizeof(float);
+    size_t totalBytes = channels * channelBytes;
+
+    float* inputDevice;
+	float* totalWeightsDevice;
+    HIP_CHECK(hipMalloc(&inputDevice, totalBytes));
+	HIP_CHECK(hipMalloc(&totalWeightsDevice, channelBytes));
+    for (int i = 0; i < channels; i++)
     {
-        int x = i % iWidth;
-        int y = i / iWidth;
-
-        // auxiliary variable
-        // number of denoised values per pixel
-        float* fpCount = new float[iwxh];
-	    for (int ii = 0; ii < iwxh; ii++)
-        {
-            fpCount[ii] = 0.0f;
-        }
-
-        // clear output
-        for (int ii = 0; ii < iChannels; ii++)
-        {
-            fpO[i + ii * iwxh] = 0.0f;
-        }
-
-        // auxiliary variable
-        // denoised patch centered at a certain pixel
-        float** fpODenoised = new float* [iChannels];
-        for (int ii = 0; ii < iChannels; ii++)
-        {
-            fpODenoised[ii] = new float[iwl];
-            for (int iii = 0; iii < iwl; iii++)
-            {
-                fpODenoised[ii][iii] = 0.0f;
-            }
-        }
-
-        // początek pętli po kolumnach
-        // reduce the size of the comparison window if we are near the boundary
-        int iDWin0 = MIN(iDWin, MIN(iWidth - 1 - x, MIN(iHeight - 1 - y, MIN(x, y))));
-
-        // research zone depending on the boundary and the size of the window
-        int imin = MAX(x - iDBloc, iDWin0);
-        int jmin = MAX(y - iDBloc, iDWin0);
-
-        int imax = MIN(x + iDBloc, iWidth - 1 - iDWin0);
-        int jmax = MIN(y + iDBloc, iHeight - 1 - iDWin0);
-
-        // maximum of weights. Used for reference patch
-        float fMaxWeight = 0.0f;
-
-        // sum of weights
-        float fTotalWeight = 0.0f;
-
-        for (int j = jmin; j <= jmax; j++)
-        {
-            for (int i = imin; i <= imax; i++)
-            {
-                if (i != x || j != y)
-                {
-                    float fDif = fiL2FloatDist(fpI, fpI, x, y, i, j, iDWin0, iChannels, iWidth, iWidth, iwxh);
-
-                    // dif^2 - 2 * fSigma^2 * N      dif is not normalized
-                    fDif = MAX(fDif - 2.0f * (float)icwl * fSigma2, 0.0f);
-                    fDif = fDif / fH2;
-
-                    float fWeight = wxSLUT(fDif, fpLut);
-
-                    if (fWeight > fMaxWeight)
-                    {
-                        fMaxWeight = fWeight;
-                    }
-
-                    fTotalWeight += fWeight;
-
-                    for (int is = -iDWin0; is <= iDWin0; is++)
-                    {
-                        int aiindex = (iDWin + is) * ihwl + iDWin;
-                        int ail = (j + is) * iWidth + i;
-
-                        for (int ir = -iDWin0; ir <= iDWin0; ir++)
-                        {
-                            int iindex = aiindex + ir;
-                            int il = ail + ir;
-
-                            for (int ii = 0; ii < iChannels; ii++)
-                            {
-                                fpODenoised[ii][iindex] += fWeight * fpI[ii * ihwl + il];
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        // current patch with fMaxWeight
-        for (int is = -iDWin0; is <= iDWin0; is++)
-        {
-            int aiindex = (iDWin + is) * ihwl + iDWin;
-            int ail = (y + is) * iWidth + x;
-            for (int ir = -iDWin0; ir <= iDWin0; ir++)
-            {
-                int iindex = aiindex + ir;
-                int il = ail + ir;
-
-                for (int ii = 0; ii < iChannels; ii++)
-                {
-                    fpODenoised[ii][iindex] += fMaxWeight * fpI[ii * ihwl + il];
-                }
-            }
-        }
-
-        fTotalWeight += fMaxWeight;
-
-        // normalize average value when fTotalweight is not near zero
-        if (fTotalWeight > fTiny)
-        {
-            for (int is = -iDWin0; is <= iDWin0; is++)
-            {
-                int aiindex = (iDWin + is) * ihwl + iDWin;
-                int ail = (y + is) * iWidth + x;
-
-                for (int ir = -iDWin0; ir <= iDWin0; ir++)
-                {
-                    int iindex = aiindex + ir;
-                    int il = ail + ir;
-
-                    fpCount[il]++;
-
-                    for (int ii = 0; ii < iChannels; ii++)
-                    {
-                        fpO[ii * ihwl + il] += fpODenoised[ii][iindex] / fTotalWeight;
-                    }
-                }
-            }
-        }
-        // koniec pętli po kolumnach
-
-        for (int ii = 0; ii < iChannels; ii++)
-        {
-            delete[] fpODenoised[ii];
-        }
-        delete[] fpODenoised;
-
-        // koniec pętli po wierszach
-
-        for (int ii = 0; ii < iwxh; ii++)
-        {
-            if (fpCount[ii] > 0.0)
-            {
-                for (int jj = 0; jj < iChannels; jj++)
-                {
-                    fpO[jj * iwxh + ii] /= fpCount[ii];
-                }
-            }
-            else
-            {
-                for (int jj = 0; jj < iChannels; jj++)
-                {
-                    fpO[jj * iwxh + ii] = fpI[jj * iwxh + ii];
-                }
-            }
-        }
-
-        delete[] fpCount;
-    }
-}
-
-extern void Denoise(int iDWin, int iDBloc, float fSigma, float fFiltPar, float** fpI, float** fpO, int iChannels, int iWidth, int iHeight)
-{
-	// length of each channel
-	int iwxh = iWidth * iHeight;
-
-    //  length of comparison window
-    int ihwl = (2 * iDWin + 1);
-    int iwl = (2 * iDWin + 1) * (2 * iDWin + 1);
-    int icwl = iChannels * iwl;
-
-    // filtering parameter
-    float fSigma2 = fSigma * fSigma;
-    float fH = fFiltPar * fSigma;
-    float fH2 = fH * fH;
-
-    // multiply by size of patch, since distances are not normalized
-    fH2 *= (float)icwl;
-
-    // tabulate exp(-x), faster than using directly function expf
-    int lutLength = (int)rintf((float)LUTMAX * (float)LUTPRECISION);
-    float* hostLut = new float[lutLength];
-    for (int i = 0; i < lutLength; i++)
-	{
-		hostLut[i] = expf(-(float)i / LUTPRECISION);
-	}
-
-    dim3 gpuThreads(256, 1, 1);
-    dim3 gpuBlocks((iwxh + 256 - 1) / 256, 1, 1);
-
-    size_t channelBytes = iwxh * sizeof(float);
-    size_t totalBytes = iChannels * channelBytes;
-    float* inputData;
-    float* outputData;
-    float* deviceLut;
-    HIP_CHECK(hipMalloc(&inputData, totalBytes));
-    HIP_CHECK(hipMalloc(&outputData, totalBytes));
-    HIP_CHECK(hipMalloc(&deviceLut, lutLength));
-    for (int i = 0; i < iChannels; i++)
-    {
-        HIP_CHECK(hipMemcpy(inputData + i * iwxh, fpI[i], channelBytes, hipMemcpyHostToDevice));
+        HIP_CHECK(hipMemcpy(inputDevice + i * channelLength, input[i], channelBytes, hipMemcpyHostToDevice));
     }
 
-    HIP_CHECK(hipMemcpy(deviceLut, hostLut, lutLength, hipMemcpyHostToDevice));
-
-    hipLaunchKernelGGL(theKernelOfShit, gpuBlocks, gpuThreads, 0, 0, iDWin, iDBloc, fSigma, fFiltPar, inputData, outputData, iChannels, iWidth, iHeight, deviceLut);
+    hipLaunchKernelGGL(getGlobalWeightsKernel, gpuBlocks, gpuThreads, 0, 0, totalWeightsDevice, windowRadius, blockRadius, sigma, fFiltPar, inputDevice, channels, width, height);
     HIP_CHECK(hipGetLastError());
 
-    for (int i = 0; i < iChannels; i++)
+	HIP_CHECK(hipMemcpy(totalWeights, totalWeightsDevice, channelBytes, hipMemcpyDeviceToHost));
+
+    HIP_CHECK(hipFree(inputDevice));
+	HIP_CHECK(hipFree(totalWeightsDevice));
+}
+
+void getItDone(int windowRadius, int blockRadius, float sigma, float fFiltPar, float** input, float** output, int channels, int width, int height, float* totalWeights)
+{
+	int channelLength = width * height;
+
+	dim3 gpuThreads(256, 1, 1);
+    dim3 gpuBlocks((channelLength + 256 - 1) / 256, 1, 1);
+
+	size_t channelBytes = channelLength * sizeof(float);
+    size_t totalBytes = channels * channelBytes;
+
+    float* inputDevice;
+	float* outputDevice;
+	float* totalWeightsDevice;
+    HIP_CHECK(hipMalloc(&inputDevice, totalBytes));
+	HIP_CHECK(hipMalloc(&outputDevice, totalBytes));
+	HIP_CHECK(hipMalloc(&totalWeightsDevice, channelBytes));
+
+	HIP_CHECK(hipMemcpy(totalWeightsDevice, totalWeights, channelBytes, hipMemcpyHostToDevice));
+    for (int i = 0; i < channels; i++)
     {
-        HIP_CHECK(hipMemcpy(fpO[i], outputData + i * iwxh, channelBytes, hipMemcpyDeviceToHost));
+        HIP_CHECK(hipMemcpy(inputDevice + i * channelLength, input[i], channelBytes, hipMemcpyHostToDevice));
     }
 
-    HIP_CHECK(hipFree(inputData));
-    HIP_CHECK(hipFree(outputData));
-    HIP_CHECK(hipFree(deviceLut));
-    delete[] hostLut;
+    hipLaunchKernelGGL(getItDoneKernel, gpuBlocks, gpuThreads, 0, 0, windowRadius, blockRadius, sigma, fFiltPar, inputDevice, outputDevice, channels, width, height, totalWeightsDevice);
+    HIP_CHECK(hipGetLastError());
+
+	for (int i = 0; i < channels; i++)
+    {
+        HIP_CHECK(hipMemcpy(output[i], outputDevice + i * channelLength, channelBytes, hipMemcpyDeviceToHost));
+    }
+
+    HIP_CHECK(hipFree(inputDevice));
+	HIP_CHECK(hipFree(outputDevice));
+	HIP_CHECK(hipFree(totalWeightsDevice));
+}
+
+extern void Denoise(int windowRadius, int blockRadius, float sigma, float fFiltPar, float** input, float** output, int channels, int width, int height)
+{
+	int channelLength = width * height;
+
+	float* totalWeights = new float[channelLength];
+
+	getGlobalWeights(totalWeights, windowRadius, blockRadius, sigma, fFiltPar, input, channels, width, height);
+	getItDone(windowRadius, blockRadius, sigma, fFiltPar, input, output, channels, width, height, totalWeights);
+
+	delete[] totalWeights;
 }
